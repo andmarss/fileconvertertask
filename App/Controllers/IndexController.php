e45176f73f57244e0c3f58be9d75ca97ee33c8e7
Марков Андрей Игоревич <anrey-markov@mail.ru>
2019-07-27 16:12:43 +0300

    Adding controller

---
+++ App/Controllers/IndexController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controllers;
+
+use App\System\Request;
+
+class IndexController
+{
+    public function index(Request $request)
+    {
+
+    }
+
+    public function upload(Request $request)
+    {
+
+    }
+}
